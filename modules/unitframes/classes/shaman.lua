4449e8b1a8273a5a93f13860ba48c223d9dd16e9
pedrozc90 <pedrozc90@gmail.com>
2020-09-13 21:29:20 -0300

    update shaman totems frame

--- modules/unitframes/classes/shaman.lua
+++ modules/unitframes/classes/shaman.lua
@@ -1,76 +1,60 @@
 local T, C, L = Tukui:unpack()
 local UnitFrames = T.UnitFrames
 local Class = select(2, UnitClass("player"))
+local MAX_TOTEMS = MAX_TOTEMS
 
 ----------------------------------------------------------------
 -- Shaman Class Resources
 ----------------------------------------------------------------
 if (Class ~= "SHAMAN") then return end
 
-UnitFrames.TotemColors = {
-	[1] = { 0.58, 0.23, 0.10 },
-	[2] = { 0.23, 0.45, 0.13 },
-	[3] = { 0.19, 0.48, 0.60 },
-	[4] = { 0.42, 0.18, 0.74 }
-}
+local baseShaman = UnitFrames.AddClassFeatures["SHAMAN"]
 
-local function Player(self)
+UnitFrames.AddClassFeatures["SHAMAN"] = function(self)
 
-	if (not C.UnitFrames.TotemBar) then return end
+    if (not C.UnitFrames.TotemBar) then return end
     
+    -- first, call the base function
+    baseShaman(self)
+    
+    -- second, we edit it
     local Totems = self.Totems
     local Shadow = self.Shadow
 
-    local Spacing = 3
-
-	local FrameWidth, FrameHeight = unpack(C["Units"].Player or { 254, 31 })
+    local FrameWidth, FrameHeight = unpack(C["Units"].Player or { 254, 31 })
     local HealthTexture = T.GetTexture(C["Textures"].UFHealthTexture)
 	
-	Totems = CreateFrame("Frame", "TukuiTotemBar", self)
+	Totems:ClearAllPoints()
 	Totems:SetPoint("BOTTOMLEFT", self, "TOPLEFT", -2, 5)
-	-- Totems:SetFrameStrata(self.Health:GetFrameStrata())
-	-- Totems:SetFrameLevel(self.Health:GetFrameLevel() + 3)
-	Totems:SetSize(FrameWidth, 7)
-    Totems:CreateBackdrop()
-    Totems.Backdrop:SetTripleBorder()
-    Totems.Backdrop:SetOutside(nil, 2, 2)
-	-- Totems.Destroy = {}
+    Totems:SetSize(FrameWidth, 7)
+    Totems.Backdrop:Kill()
     
     Shadow:Kill()
 
-	for i = 1, 4 do
-		local r, g, b = unpack(TukuiUnitFrames.TotemColors[i])
+    local Spacing = 3
+    local TotemWidth, TotemDelta = T.EqualSizes(FrameWidth, MAX_TOTEMS, Spacing)
 
-		Totems[i]:ClearAllPoints()
-		Totems[i]:CreateBackdrop()
-		Totems[i].Backdrop:SetParent(Totems)
+	for i = 1, MAX_TOTEMS do
+		local r, g, b = unpack(UnitFrames.TotemColors[i])
 
-		-- Totems[i]:SetStatusBarTexture(HealthTexture)
-		-- Totems[i]:SetStatusBarColor(r, g, b)
-		-- Totems[i]:SetMinMaxValues(0, 1)
-		-- Totems[i]:SetValue(0)
-
-		-- Totems[i].bg = Totems[i]:CreateTexture(nil, "BORDER")
-		-- Totems[i].bg:SetAllPoints(Totems[i])
-		-- Totems[i].bg:SetTexture(C.Medias.Blank)
-		-- Totems[i].bg.multiplier = 0.3
-
-		-- Totems[i].bg:SetVertexColor(r * .3, g * .3, b * .3)
+        Totems[i]:ClearAllPoints()
+        Totems[i]:SetHeight(Totems:GetHeight())
+        Totems[i]:SetWidth(TotemWidth)
+        Totems[i]:CreateBackdrop()
+        Totems[i].Backdrop:SetTripleBorder()
+        Totems[i].Backdrop:SetParent(Totems)
+        
+        if ((TotemDelta > 0) and (i <= TotemDelta)) then
+            Totems[i]:SetWidth(TotemWidth + 1)
+        else
+            Totems[i]:SetWidth(TotemWidth)
+        end
 		
 		if (i == 1) then
-			Totems[i]:Point("BOTTOMLEFT", Totems, "BOTTOMLEFT", 0, 0)
-			Totems[i]:Size(61, 8)
+			Totems[i]:Point("BOTTOMLEFT", Totems, "BOTTOMLEFT", 1, 0)
 		else
 			Totems[i]:Point("BOTTOMLEFT", Totems[i-1], "BOTTOMRIGHT", Spacing, 0)
-			Totems[i]:Size(62, 8)
 		end
-
-		-- Totems.Destroy[i] = CreateFrame("Button", Totems[i]:GetName().."Destroy", UIParent, "SecureUnitButtonTemplate")
-		-- Totems.Destroy[i]:RegisterForClicks("RightButtonUp")
-		-- Totems.Destroy[i]:SetAllPoints(Totems[i])
-		-- Totems.Destroy[i]:SetID(i)
-		-- Totems.Destroy[i]:SetAttribute("type2", "destroytotem")
-		-- Totems.Destroy[i]:SetAttribute("*totem-slot*", i)
 	end
 	
 	if (C.UnitFrames.PlayerAuras and C.UnitFrames.PlayerAuraBars) then
@@ -80,6 +64,4 @@ local function Player(self)
 		self.Buffs:ClearAllPoints()
 		self.Buffs:Point("BOTTOMLEFT", self, "TOPLEFT", 0, 15)
     end
-    
 end
-hooksecurefunc(UnitFrames, "Player", Player)
