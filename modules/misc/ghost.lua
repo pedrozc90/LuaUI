c0c6a911576fb71b37b4f87456eefdad6fd08169
pedrozc90 <pedrozc90@gmail.com>
2021-02-15 00:19:10 -0300

    update ghost button position

--- modules/misc/ghost.lua
+++ modules/misc/ghost.lua
@@ -1,6 +1,7 @@
 local T, C, L = Tukui:unpack()
 local Ghost = T.Miscellaneous.Ghost
 local GhostFrame = GhostFrame
+local Minimap = T.Maps.Minimap
 local Experience = T.Miscellaneous.Experience
 
 ----------------------------------------------------------------
@@ -21,41 +22,21 @@ function Ghost:CreateButton()
 
 	Button:ClearAllPoints()
 	-- Button:SetAllPoints(T.DataTexts.Panels.Minimap or T.DataTexts.Panels.Right)
-	Button:SetPoint("TOPLEFT", HonorBar, "BOTTOMLEFT", 0, -3)
-	Button:SetPoint("TOPRIGHT", HonorBar, "BOTTOMRIGHT", 0, -3)
+	Button:SetPoint("TOPLEFT", Minimap, "BOTTOMLEFT", 0, -3)
+	Button:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, -3)
+	Button:SetHeight(23)
+	Button:SetScript("OnShow", function (element)
+        local ExpirenceBar = Experience.XPBar1
+        local ReputationBar = Experience.XPBar2
+
+        local Anchor = Minimap
+        if (ReputationBar and ReputationBar:IsShown()) then
+            Anchor = ReputationBar
+        elseif (ExpirenceBar and ExpirenceBar:IsShown()) then
+            Anchor = ExpirenceBar
+        end
+        element:SetPoint("TOPLEFT", Anchor, "BOTTOMLEFT", 0, -3)
+        element:SetPoint("TOPRIGHT", Anchor, "BOTTOMRIGHT", 0, -3)
+    end)
 	Button:Hide()
-	
-	-- Button.Text:SetFontTemplate(C.Medias.Font, 12)
-	-- Button.Text:SetPoint("CENTER")
-	-- Button.Text:SetText(T.RGBToHex(unpack(Ghost.Color)) .. RETURN_TO_GRAVEYARD .. "|r")
 end
-
--- function Ghost:AddHooks()
--- 	GhostFrame:HookScript("OnShow", Ghost.OnShow)
--- 	GhostFrame:HookScript("OnHide", Ghost.OnHide)
--- end
-
--- function Ghost:Enable()
--- 	local DataRight = T.DataTexts.Panels.Right
--- 	local Minimap = T.DataTexts.Panels.Minimap
--- 	local Icon = GhostFrameContentsFrame
--- 	local Text = GhostFrameContentsFrameText
-
--- 	if Minimap or DataRight then
--- 		self:CreateButton()
--- 		self:AddHooks()
-
--- 		GhostFrame:StripTextures()
--- 		GhostFrame:ClearAllPoints()
--- 		GhostFrame:SetAllPoints(Minimap or DataRight)
--- 		GhostFrame:SetFrameStrata(self.Button:GetFrameStrata())
--- 		GhostFrame:SetFrameLevel(self.Button:GetFrameLevel() + 1)
--- 		GhostFrame:SetAlpha(0)
-
--- 		Icon:StripTextures()
-
--- 		Text:SetText("")
--- 	end
--- end
-
--- Miscellaneous.Ghost = Ghost
