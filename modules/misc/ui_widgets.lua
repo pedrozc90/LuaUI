383efa8ecf58bf1d34418398b0a92200822623f1
pedrozc90 <pedrozc90@gmail.com>
2021-06-29 21:51:34 -0300

    adding ui-widget skip

---
+++ modules/misc/ui_widgets.lua
@@ -0,0 +1,29 @@
+local T, C, L = Tukui:unpack()
+local UIWidgets = T.Miscellaneous.UIWidgets
+
+----------------------------------------------------------------
+-- UI Widgets
+----------------------------------------------------------------
+-- local baseSkinUIWidgetStatusBar = UIWidgets.SkinUIWidgetStatusBar
+local baseEnable = UIWidgets.Enable
+
+-- function UIWidgets:SkinUIWidgetStatusBar(widgetInfo, widgetContainer)
+
+--     -- first, we call the base function
+--     baseSkinUIWidgetStatusBar(self, widgetInfo, widgetContainer)
+
+--     -- second, we edit it
+--     local Texture = T.GetTexture(C.Lua.Texture)
+
+-- end
+
+function UIWidgets:Enable()
+
+    -- first, we call the base function
+    baseEnable(self)
+
+    -- second, we edit it
+    self.Holder:ClearAllPoints()
+	self.Holder:SetSize(220, 20)
+	self.Holder:SetPoint("TOP", UIParent, "TOP", 3, -96)
+end
