3fb12b7af28c5b28ba1a8c6a8336c31f3e2afdea
pedrozc90 <pedrozc90@gmail.com>
2024-08-24 00:02:36 -0300

    fix action bar 2 button order

--- modules/actionbars/bar2.lua
+++ modules/actionbars/bar2.lua
@@ -30,66 +30,70 @@ function ActionBars:CreateBar2()
 
     local Width, Height = ActionBars.GetBackgroundSize(ButtonsPerRow, NumRow, Size, Spacing, C.ActionBars.ShowBackdrop)
 
-    if (ActionBar2) then
-        ActionBar2:Kill()
-    end
+    -- if (ActionBar2) then
+    --     ActionBar2:Kill()
+    -- end
 
-    local ActionBar2Left = CreateFrame("Frame", "TukuiActionBar2Left", T.PetHider, "SecureHandlerStateTemplate")
-    ActionBar2Left:SetPoint("BOTTOMRIGHT", ActionBar1, "BOTTOMLEFT", -Spacing, 0)
-    ActionBar2Left:SetFrameStrata("LOW")
-    ActionBar2Left:SetFrameLevel(10)
-    ActionBar2Left:SetWidth(Width)
-    ActionBar2Left:SetHeight(Height)
+    local Left = CreateFrame("Frame", ActionBar2:GetName() .. "Left", ActionBar2)
+    Left:SetPoint("BOTTOMRIGHT", ActionBar1, "BOTTOMLEFT", -Spacing, 0)
+    Left:SetFrameStrata("LOW")
+    Left:SetFrameLevel(10)
+    Left:SetWidth(Width)
+    Left:SetHeight(Height)
+    ActionBar2.Left = Left
 
-    local ActionBar2Right = CreateFrame("Frame", "TukuiActionBar2Right", T.PetHider, "SecureHandlerStateTemplate")
-    ActionBar2Right:SetPoint("BOTTOMLEFT", ActionBar1, "BOTTOMRIGHT", Spacing, 0)
-    ActionBar2Right:SetFrameStrata("LOW")
-    ActionBar2Right:SetFrameLevel(10)
-    ActionBar2Right:SetWidth(Width)
-    ActionBar2Right:SetHeight(Height)
+    local Right = CreateFrame("Frame", ActionBar2:GetName() .. "Right", ActionBar2)
+    Right:SetPoint("BOTTOMLEFT", ActionBar1, "BOTTOMRIGHT", Spacing, 0)
+    Right:SetFrameStrata("LOW")
+    Right:SetFrameLevel(10)
+    Right:SetWidth(Width)
+    Right:SetHeight(Height)
+    ActionBar2.Right = Right
+
+    if ActionBar2.Backdrop then ActionBar2.Backdrop:Kill() end
+    if ActionBar2.Shadow then ActionBar2.Shadow:Kill() end
 
     if (C.ActionBars.ShowBackdrop) then
-        ActionBar2Left:CreateBackdrop("Transparent")
-        ActionBar2Right:CreateBackdrop("Transparent")
+        Left:CreateBackdrop("Transparent")
+        Right:CreateBackdrop("Transparent")
     end
 
-    if (not T.Retail) then
-        MultiBarBottomLeft:SetShown(true)
-    else
-        Settings.SetValue("PROXY_SHOW_ACTIONBAR_2", true)
-    end
+    -- if (not T.Retail) then
+    --     MultiBarBottomLeft:SetShown(true)
+    -- else
+    --     Settings.SetValue("PROXY_SHOW_ACTIONBAR_2", true)
+    -- end
 
     -- MultiBarBottomLeft:SetShown(true)
-    MultiBarBottomLeft:SetParent(ActionBar2Left)
+    -- MultiBarBottomLeft:SetParent(ActionBar2Left)
 
     for i = 1, NUM_ACTIONBAR_BUTTONS do
-        local Button = _G["MultiBarBottomLeftButton" .. i]
-
+        local Button = ActionBar2["Button" .. i]
         Button:SetSize(Size, Size)
         Button:ClearAllPoints()
         Button:SetAttribute("showgrid", 1)
         -- Button:ShowGrid(ACTION_BUTTON_SHOW_GRID_REASON_EVENT)
 
-        ActionBars:SkinButton(Button)
+        -- ActionBars:SkinButton(Button)
 
         if (i == 1) then
-            Button:SetPoint("BOTTOMRIGHT", ActionBar2Left, "BOTTOMRIGHT", -Padding, Padding)
+            Button:SetPoint("TOPLEFT", Left, "TOPLEFT", Padding, -Padding)
         elseif (i == 7) then
-            Button:SetPoint("BOTTOMRIGHT", ActionBar2Right, "BOTTOMRIGHT", -Padding, Padding)
+            Button:SetPoint("TOPLEFT", Right, "TOPLEFT", Padding, -Padding)
         else
-            local PreviousButton = _G["MultiBarBottomLeftButton" .. (i-1)]
-            Button:SetPoint("RIGHT", PreviousButton, "LEFT", -Spacing, 0)
+            local PreviousButton = ActionBar2["Button" .. (i - 1)]
+            Button:SetPoint("LEFT", PreviousButton, "RIGHT", Spacing, 0)
         end
 
         ActionBar2["Button" .. i] = Button
     end
 
-    RegisterStateDriver(ActionBar2, "visibility", "[vehicleui] hide; show")
+    -- RegisterStateDriver(ActionBar2, "visibility", "[vehicleui] hide; show")
 
-    Movers:RegisterFrame(ActionBar2Left, "Action Bar Left #2")
-    Movers:RegisterFrame(ActionBar2Right, "Action Bar Right #2")
+    -- Movers:RegisterFrame(Left, "Action Bar Left #2")
+    -- Movers:RegisterFrame(ActionBar2Right, "Action Bar Right #2")
     Movers.Defaults["TukuiActionBar2"] = nil
 
-    self.Bars.Bar2Left = ActionBar2Left
-    self.Bars.Bar2Right = ActionBar2Right
+    -- self.Bars.Bar2Left = ActionBar2Left
+    -- self.Bars.Bar2Right = ActionBar2Right
 end
